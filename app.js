7b093d98f0962ec3bba74297dae626b9f452aa03
Pulasthi Peiris <pulasthipeiris22@gmail.com>
2020-12-07 19:46:12 -0500

    Add existing files

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -12,25 +15,35 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
-    let today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
+    let day = date();
 
     res.render('list', {
-        kindOfDay: day,
+        listTitle: day,
         newListItems: items
         });
 });
 
+app.get("/work", function(req, res){
+
+    res.render('list', {
+        listTitle: "Work List",
+        newListItems: workItems
+        });
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
 app.post("/", function(req, res){
+
     let item = req.body.newItem;
-    
+
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work")
+    } 
+
     items.push(item);
 
     res.redirect("/");
